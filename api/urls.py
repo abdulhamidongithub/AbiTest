4a7ffb2db9371ed7ece990805a91e351a9aecd22
abdulhamidongithub <1997abdulhamid@gmail.com>
2024-04-04 10:44:03 +0900

    TestsView added

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path("subjects/", SubjectsAPIView.as_view()),
+    path("subject/<str:pk>/tests/", SubjectTests.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 
 from .models import *
@@ -9,5 +10,11 @@ class SubjectsAPIView(APIView):
     def get(self, request):
         subjects = Subject.objects.all()
         serializer = SubjectSerializer(subjects, many=True)
+        return Response(serializer.data)
+
+class SubjectTests(APIView):
+    def get(self, request, pk):
+        tests = Test.objects.filter(subject__id=pk)
+        serializer = TestSerializer(tests, many=True)
         return Response(serializer.data)
 
