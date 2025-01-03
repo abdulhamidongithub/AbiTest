fdc4509a095415cfd4539843353a8f00d6d9dc42
abdulhamidongithub <1997abdulhamid@gmail.com>
2025-01-03 21:04:48 +0900

    RefreshToken added

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from .views import *
 
 urlpatterns = [
     path('get_token/', MyTokenObtainPairView.as_view()),
+    path('refresh_token/', TokenRefreshView.as_view()),
 
     path("subjects/", SubjectsAPIView.as_view()),
     path("subject/<int:pk>/random_test/", SubjectRandomTest.as_view()),
