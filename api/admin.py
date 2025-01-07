d42c7a0281fef2219cde1a4c7facbd020b167272
abdulhamidongithub <1997abdulhamid@gmail.com>
2025-01-07 21:53:36 +0900

    Models registered to admin

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,41 @@
 from django.contrib import admin
 
-from .models import *
+from .models import (
+Candidate, Major, Subject,
+Author, Test, UserTest,
+Question, UserAnswer
+)
 
-admin.site.register(Subject)
+@admin.register(Candidate)
+class CandidateAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Major)
+class MajorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(UserTest)
+class UserTestAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(UserAnswer)
+class UserAnswerAdmin(admin.ModelAdmin):
+    pass
+
+
